4998a6afbd816c5061ca7357efe0d2942c523aa9
Victor Manuel Guayara Suarez <61199510+guayara504@users.noreply.github.com>
2022-03-15 17:22:56 -0500

    Update UnixExcel.py

--- UnixExcel.py
+++ UnixExcel.py
@@ -1,9 +1,18 @@
+from Consultor import *
 from Driver import *
-actuaciones = []
-
+from Extractor import *
 #Clase principal   
 if __name__ == "__main__":
+    actuaciones = []
     datos = {"fecha": "","despacho":"","ponente":"","tipo_proceso":"","clase_proceso":"","demandante":"","demandado":""}
-    driver = Driver_1()
+    browser =Driver_1()
+    consulta =Consultor_1
+    extractor = Extractor_1
+    consulta.iniciar_busqueda((browser.driver))
+    consulta.ingresar_radicado((browser.driver))
+    consulta.click_Proceso((browser.driver))
+    extractor.extraer_datos(datos,(browser.driver))
+    extractor.extraer_partes(datos,(browser.driver))
+    extractor.extraer_actuaciones(actuaciones,(browser.driver))
     for dato in datos:
         print(dato+":",datos[dato])
