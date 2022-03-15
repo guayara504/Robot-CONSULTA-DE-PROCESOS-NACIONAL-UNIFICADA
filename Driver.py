15b555f5caa21e6637cc9a0f0305b411e1390328
Victor Manuel Guayara Suarez <61199510+guayara504@users.noreply.github.com>
2022-03-15 17:22:17 -0500

    Update Driver.py

--- Driver.py
+++ Driver.py
@@ -17,46 +17,26 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support import expected_conditions as EC
 
 class Driver_1(object):
 
+    #Declaracion de Variables
     op = webdriver.ChromeOptions()
     op.add_experimental_option('excludeSwitches', ['enable-logging'])
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=op)
 
-
-    def __init__(self):
-        #Escoger preferencias del WebDriver
-        self.delay = 4
+    #Escoger preferencias del WebDriver
+    def __init__(self):  
         self.driver.set_window_size(1024, 768)
-        self.driver.implicitly_wait(3)
         self.load_page()
         
     #Cargar la pagina solicitada
-
     def load_page(self):
-        wait = WebDriverWait(self.driver, self.delay)
         self.driver.get("https://consultaprocesos.ramajudicial.gov.co/Procesos/NumeroRadicacion/")
         os.system ("cls")
-        
         try:
-            wait.until(self.driver.find_element(By.XPATH,'//*[@id="mainContent"]/div/div/div/div[1]/div/div[2]/span'))
+            WebDriverWait(self.driver,timeout=4).until(EC.presence_of_element_located((By.XPATH,'//*[@id="mainContent"]/div/div/div/div[1]/div/div[2]/span')))
         except TimeoutException:
             print('line: 38 error: No se cargo la pagina, TimeoutException')
     
-    
-
-    #Espera por el elemento
-    def WaitForElement(self,path):
-        demora = 3
-        limit = demora
-        inc = 1
-        c = 0
-        while c < limit:
-            try:
-                self.driver.find_element(By.XPATH,path)
-                return 1
-            except:
-                time.sleep(inc)
-                c+=inc
-        return 0
